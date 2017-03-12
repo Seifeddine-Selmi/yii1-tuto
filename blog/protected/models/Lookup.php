a85fb4de330e15904ded4538b7b1a37dc151e531
Seifeddine-Selmi <selmi.seifeddine19@gmail.com>
2017-03-11 22:05:10 -0500

    Set time and user before save new posts

--- blog/protected/models/Lookup.php
+++ blog/protected/models/Lookup.php
@@ -104,6 +104,13 @@ class Lookup extends CActiveRecord
 		return parent::model($className);
 	}
 
+
+	/**
+	 * Returns the items for the specified type.
+	 * @param string item type (e.g. 'PostStatus').
+	 * @return array item names indexed by item code. The items are order by their position values.
+	 * An empty array is returned if the item type does not exist.
+	 */
 	public static function items($type)
 	{
 		if(!isset(self::$_items[$type]))
@@ -111,6 +118,12 @@ class Lookup extends CActiveRecord
 		return self::$_items[$type];
 	}
 
+	/**
+	 * Returns the item name for the specified type and code.
+	 * @param string the item type (e.g. 'PostStatus').
+	 * @param integer the item code (corresponding to the 'code' column value)
+	 * @return string the item name for the specified the code. False is returned if the item type or code does not exist.
+	 */
 	public static function item($type,$code)
 	{
 		if(!isset(self::$_items[$type]))
@@ -118,6 +131,10 @@ class Lookup extends CActiveRecord
 		return isset(self::$_items[$type][$code]) ? self::$_items[$type][$code] : false;
 	}
 
+	/**
+	 * Loads the lookup items for the specified type from the database.
+	 * @param string the item type
+	 */
 	private static function loadItems($type)
 	{
 		self::$_items[$type]=array();
